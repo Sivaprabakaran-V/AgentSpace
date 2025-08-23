66775f185dbcc6c22d3a13781a11a55560219aa5
Sivaprabakaran-V <sivaprabakaran02@gmail.com>
2025-08-23 21:24:38 +0530

    finished the search agent

--- Linkedin_Agent.py
+++ Linkedin_Agent.py
@@ -5,30 +5,38 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
 import os
 from third_parties.linkedin_search import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    load_dotenv()
+load_dotenv()
 
-    summary_template = """Given the Linkedin information {information} about a person, I want you to create:
-    1. A short summary
-    2. Two interesting facts about them"""
+def linkedin(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_url=linkedin_username)    
 
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], 
-        template=summary_template
-    )
+summary_template = """
+given the Linkedin information {information} about a person I want you to create:
+1. A short summary
+2. two interesting facts about them
+"""
+summary_prompt_template = PromptTemplate(
+    input_variables=["information"], template=summary_template
+)
 
-    # Use Google Gemini (Free via Google AI Studio)
-    llm = ChatGoogleGenerativeAI(
+llm = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",  # or "gemini-1.5-pro" for higher quality
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0
     )
 
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(linkedin_url="https://www.linkedin.com/in/alliekmiller/")
+chain = summary_prompt_template | llm
 
-    res = chain.invoke(input={"information": linkedin_data})
-    print(res)
+res = chain.invoke(input={"information": linkedin_data})
 
-    print("This is the end of the chaining single prompt example.")
+print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Linkedin Agenet enter")
+    linkedin(name="Allie Miller")
